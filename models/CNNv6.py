436de19314cf5fd47df3fcc8f8725426012466f6
DaoCuong <Cuongsieunhancsn@gmail.com>
2022-12-02 22:22:55 +0700

    update nonFL

--- models/CNNv6.py
+++ models/CNNv6.py
@@ -8,12 +8,11 @@ def create_keras_model(NUM_FEATURE, NUM_CLASSES):
     model = Sequential()
     model.add(Conv1D(input_shape=(NUM_FEATURE, 1), filters=32,
               kernel_size=(1), strides=1, padding='same', activation='relu',kernel_regularizer=l2(0.1),bias_regularizer=l2(0.1)))
-    model.add(Dropout(0.5))
     model.add(MaxPool1D(pool_size=3, strides=3,padding='same'))
     model.add(Flatten())
-    model.add(Dense(32,kernel_regularizer=l2(0.1),bias_regularizer=l2(0.1)))
+    model.add(Dense(32,kernel_regularizer=l2(0.1),bias_regularizer=l2(0.1),activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(NUM_CLASSES,activation='softmax'))
+    model.add(Dense(NUM_CLASSES))
 
     # model = Sequential([
     #     Conv2D(32, (3, 3), activation='relu', input_shape=(1, 100)),
